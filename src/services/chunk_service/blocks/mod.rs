feea5c2f109fb91db3fc6901d8b8f4839595692b
daarkzek@protonmail.com <daarkzek@protonmail.com>
2021-05-24 12:19:55 +1200

    Remove outdated block.json file & uploaded shaders

--- src/services/chunk_service/blocks/mod.rs
+++ src/services/chunk_service/blocks/mod.rs
@@ -5,13 +5,6 @@ pub struct BlockManager {}
 
 impl BlockManager {
     pub fn new() -> BlockManager {
-        let data = get_data();
-
         BlockManager {}
     }
-}
-
-fn get_data() -> Value {
-    let data = include_str!("../../../../../RustCraft/assets/blocks.json");
-    serde_json::from_str(data).unwrap()
 }
