de829d8588618eef82d35df44eced2ca9c8e00a4
Miren <mirenbhakta21@gmail.com>
2022-11-16 20:53:53 -0600

    more rc_protocol cleanup

--- lib/rc_protocol/src/protocol.rs
+++ lib/rc_protocol/src/protocol.rs
@@ -1,18 +1,12 @@
 use crate::protocol::clientbound::block_update::BlockUpdate;
 use crate::protocol::clientbound::chat::ChatSent;
 use crate::protocol::clientbound::chunk_update::FullChunkUpdate;
-use crate::protocol::clientbound::ping::Ping;
-use crate::protocol::clientbound::player_join::PlayerJoin;
-use crate::protocol::clientbound::player_leave::PlayerLeave;
-use crate::protocol::serverbound::authenticate::UserAuthenticate;
 use crate::protocol::serverbound::player_move::PlayerMove;
 use crate::protocol::serverbound::player_rotate::PlayerRotate;
-use crate::protocol::serverbound::pong::Pong;
 use serde::{Serialize, Deserialize};
 use crate::protocol::clientbound::entity_moved::EntityMoved;
 use crate::protocol::clientbound::spawn_entity::SpawnEntity;
 use crate::protocol::clientbound::entity_rotated::EntityRotated;
-use crate::protocol::serverbound::disconnect::Disconnect;
 use crate::protocol::clientbound::despawn_entity::DespawnEntity;
 
 pub mod clientbound;
--- lib/rc_protocol/src/protocol/clientbound/player_leave.rs
+++
@@ -1,8 +0,0 @@
-use crate::constants::UserId;
-
-use serde::{Serialize, Deserialize};
-#[derive(Serialize, Deserialize, PartialEq, Debug, Copy, Clone)]
-#[repr(C)]
-pub struct PlayerLeave {
-    pub id: UserId,
-}
