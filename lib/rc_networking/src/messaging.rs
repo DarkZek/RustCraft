201f38ccf9f7318e98eda9a165edb498ed9a4caf
Miren <mirenbhakta21@gmail.com>
2022-11-23 23:09:56 -0600

    revise messaging once again

--- lib/rc_networking/src/messaging.rs
+++ lib/rc_networking/src/messaging.rs
@@ -28,14 +28,6 @@ impl Plugin for ProtocolPlugin {
     }
 }
 
-#[cfg(not(feature = "server"))]
-use client as inner;
-
-#[cfg(feature = "server")]
-use server as inner;
-
-use inner::*;
-
 #[macro_export]
 macro_rules! impl_message {
     ($typ: ty, $p_id: literal, c_id: literal) => {
@@ -52,22 +44,33 @@ pub trait Message: Event + Serialize + for<'a> Deserialize<'a> {
 }
 
 #[derive(SystemParam)]
-pub struct MessageSender<'w, 's, T: Message> {
-    val: EventWriter<'w, 's, SendMsg<T>>
+pub struct Sender<'w, 's, T: Event> {
+    val: EventWriter<'w, 's, T>,
 }
 
-impl<T: Message> MessageSender<'_, '_, T> {
+impl<T: Event> Sender<'_, '_, T> {
+    pub fn send(&mut self, t: impl Into<T>) {
+        self.val.send(t.into());
+    }
+
+    pub fn send_batch(&mut self, events: impl IntoIterator<Item=impl Into<T>>) {
+        let iter = events
+            .into_iter()
+            .map(|v| v.into());
+        self.val.send_batch(iter);
+    }
+
     pub fn send_default(&mut self) where T: Default {
-        self.val.send_default();
+        self.send_default();
     }
 }
 
 #[derive(SystemParam)]
-pub struct MessageReceiver<'w, 's, T: Message> {
-    val: EventReader<'w, 's, RecvMsg<T>>
+pub struct Receiver<'w, 's, T: Event> {
+    val: EventReader<'w, 's, T>,
 }
 
-impl<T: Message> MessageReceiver<'_, '_, T> {
+impl<T: Event> Receiver<'_, '_, T> {
     pub fn len(&self) -> usize {
         self.val.len()
     }
@@ -81,77 +84,112 @@ impl<T: Message> MessageReceiver<'_, '_, T> {
     }
 }
 
-#[cfg(not(feature = "server"))]
-mod client {
-    use crate::messaging::{Message, MessageReceiver, MessageSender};
+macro_rules! impl_deref {
+    ($typ: ty, $inner: ty) => {
+        impl<'w, 's, T: crate::messaging::Message> Deref for $typ {
+            type Target = $inner;
 
-    #[derive(Default)]
-    pub struct SendMsg<T>(pub T);
-
-    #[derive(Default)]
-    pub struct RecvMsg<T>(pub T);
-
-    impl<T: Message> MessageSender<'_, '_, T> {
-        pub fn send(&mut self, t: T) {
-            self.val.send(SendMsg(t));
+            fn deref(&self) -> &Self::Target {
+                &self.inner
+            }
         }
 
-        pub fn send_batch(&mut self, events: impl IntoIterator<Item=T>) {
-            self.val.send_batch(events.into_iter().map(|v| SendMsg(v)));
-        }
-    }
-
-    impl<T: Message> MessageReceiver<'_, '_, T> {
-        pub fn iter(&mut self) -> impl DoubleEndedIterator + ExactSizeIterator + '_ {
-            self.val.iter().map(|v| &v.0)
-        }
-
-        pub fn iter_with_id(&mut self) -> impl DoubleEndedIterator + ExactSizeIterator + '_ {
-            self.val.iter_with_id().map(|(v, e)| (&v.0, e))
+        impl<'w, 's, T: crate::messaging::Message> DerefMut for $typ {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.inner
+            }
         }
     }
 }
 
-#[cfg(feature = "server")]
-mod server {
+pub mod client {
+    use std::ops::{Deref, DerefMut};
     use bevy::ecs::event::EventId;
-    use crate::messaging::{Message, MessageReceiver, MessageSender};
+    use bevy::ecs::system::SystemParam;
+    use crate::messaging::Message;
+    use crate::messaging::{Receiver, Sender};
 
-    #[derive(Default)]
-    pub struct SendMsg<T>(pub T, pub u64);
+    pub struct SendMsg<T>(pub T);
 
-    #[derive(Default)]
-    pub struct RecvMsg<T>(pub T, pub u64);
-
-    impl<T: Message> MessageSender<'_, '_, T> {
-        pub fn send(&mut self, t: T, client: impl Into<u64>) {
-            self.val.send(SendMsg(t, client.into()))
-        }
-
-        pub fn send_batch(&mut self, events: impl IntoIterator<Item=(T, impl Into<u64>)>) {
-            let iter = events
-                .into_iter()
-                .map(|(v, e)| {
-                    SendMsg(v, e.into())
-                });
-            self.val.send_batch(iter);
+    impl<T> From<T> for SendMsg<T> {
+        fn from(value: T) -> Self {
+            Self(value)
         }
     }
 
+    pub struct RecvMsg<T>(pub T);
+
+    #[derive(SystemParam)]
+    pub struct MessageSender<'w, 's, T: Message> {
+        inner: Sender<'w, 's, SendMsg<T>>,
+    }
+
+    impl_deref!(MessageSender<'w, 's, T>, Sender<'w, 's, SendMsg<T>>);
+
+    #[derive(SystemParam)]
+    pub struct MessageReceiver<'w, 's, T: Message> {
+        inner: Receiver<'w, 's, RecvMsg<T>>,
+    }
+
+    impl_deref!(MessageReceiver<'w, 's, T>, Receiver<'w, 's, RecvMsg<T>>);
+
     impl<T: Message> MessageReceiver<'_, '_, T> {
-        pub fn iter(&mut self) -> impl DoubleEndedIterator<Item=(&T, u64)> + ExactSizeIterator<Item=(&T, u64)> + '_ {
+        pub fn iter(&mut self) -> impl DoubleEndedIterator<Item=&T> + ExactSizeIterator + '_ {
             self.val
                 .iter()
-                .map(|v| {
-                    (&v.0, v.1)
-                })
+                .map(|v| &v.0)
         }
 
-        pub fn iter_with_id(&mut self) -> impl DoubleEndedIterator<Item=(&T, u64, EventId<RecvMsg<T>>)> + ExactSizeIterator<Item=(&T, u64, EventId<RecvMsg<T>>)> + '_ {
-            self.val.iter_with_id()
-                .map(|(v, e)| {
-                    (&v.0, v.1, e)
-                })
+        pub fn iter_with_id(&mut self) -> impl DoubleEndedIterator<Item=(&T, EventId<RecvMsg<T>>)> + ExactSizeIterator + '_ {
+            self.val
+                .iter_with_id()
+                .map(|(v, e)| (&v.0, e))
+        }
+    }
+}
+
+pub mod server {
+    use std::ops::{Deref, DerefMut};
+    use bevy::ecs::event::EventId;
+    use bevy::ecs::system::SystemParam;
+    use crate::messaging::Message;
+    use crate::messaging::{Receiver, Sender};
+
+    pub struct SendMsg<T>(pub T, pub u64);
+
+    impl<T> From<(T, u64)> for SendMsg<T> {
+        fn from(value: (T, u64)) -> Self {
+            Self(value.0, value.1)
+        }
+    }
+
+    pub struct RecvMsg<T>(pub T, pub u64);
+
+    #[derive(SystemParam)]
+    pub struct MessageSender<'w, 's, T: Message> {
+        inner: Sender<'w, 's, SendMsg<T>>,
+    }
+
+    impl_deref!(MessageSender<'w, 's, T>, Sender<'w, 's, SendMsg<T>>);
+
+    #[derive(SystemParam)]
+    pub struct MessageReceiver<'w, 's, T: Message> {
+        inner: Receiver<'w, 's, RecvMsg<T>>,
+    }
+
+    impl_deref!(MessageReceiver<'w, 's, T>, Receiver<'w, 's, RecvMsg<T>>);
+
+    impl<T: Message> MessageReceiver<'_, '_, T> {
+        pub fn iter(&mut self) -> impl DoubleEndedIterator<Item=(&T, u64)> + ExactSizeIterator + '_ {
+            self.val
+                .iter()
+                .map(|v| (&v.0, v.1))
+        }
+
+        pub fn iter_with_id(&mut self) -> impl DoubleEndedIterator<Item=(&T, u64, EventId<RecvMsg<T>>)> + ExactSizeIterator + '_ {
+            self.val
+                .iter_with_id()
+                .map(|(v, e)| (&v.0, v.1, e))
         }
     }
 }
