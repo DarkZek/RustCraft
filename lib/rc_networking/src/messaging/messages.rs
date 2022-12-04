52e4fb8d49aeaf3ee4a9f01ec73dbef54a3734c4
Miren <mirenbhakta21@gmail.com>
2022-12-04 16:49:48 -0600

    fix errors in messages.rs

--- lib/rc_networking/src/messaging/messages.rs
+++ lib/rc_networking/src/messaging/messages.rs
@@ -1,6 +1,18 @@
 use bevy::prelude::*;
+use serde::{Deserialize, Serialize};
 use crate::client::Client;
 use crate::server::Server;
-use crate::messaging::serialize::make_deserializers;
+use crate::config::Channel;
+use crate::messaging::serialize::make_serializer;
+use crate::impl_message;
 
-make_deserializers![];
+make_serializer![
+    BlockUpdate
+];
+
+#[derive(Serialize, Deserialize)]
+pub struct BlockUpdate {
+    pub id: u32,
+    pub pos: IVec3,
+}
+impl_message!(BlockUpdate, 10, Channel::RELIABLE);
