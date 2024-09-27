1a45cd9b75296dd44d231963ae880fd7feda1dbe
Marshall Scott <daarkzek@protonmail.com>
2024-09-27 23:59:22 +1200

    Add give command

--- server/src/game/commands/mod.rs
+++ server/src/game/commands/mod.rs
@@ -7,7 +7,10 @@ use rc_networking::protocol::clientbound::game_object_moved::GameObjectMoved;
 use rc_networking::protocol::Protocol;
 use rc_networking::types::SendPacket;
 use rc_shared::constants::UserId;
+use rc_shared::item::ItemStates;
+use rc_shared::item::types::ItemStack;
 use crate::game::entity::DirtyPosition;
+use crate::game::inventory::Inventory;
 use crate::game::transform::Transform;
 use crate::game::world::data::WorldData;
 use crate::game::world::WORLD_SPAWN_LOCATION;
@@ -54,6 +57,9 @@ pub fn execute_commands(
             "spawn" => {
                 parse_spawn(args, command.user_id, world)
             }
+            "give" => {
+                parse_give(args, command.user_id, world)
+            }
             &_ => format!("Unknown command <{}>", command_name)
         };
 
@@ -93,4 +99,62 @@ fn parse_spawn(command: Vec<String>, user_id: UserId, world: &mut World) -> Stri
     ));
 
     format!("Returned to spawn")
+}
+
+fn parse_give(command: Vec<String>, user_id: UserId, world: &mut World) -> String {
+    if command.len() != 4 {
+        return format!("Incorrect arguments for command <{}> Usage: /give DarkZek mcv3::ImprovisedFurnaceItem 64", command.get(0).unwrap());
+    }
+
+    let target_user = command.get(1).unwrap();
+    let target_item = command.get(2).unwrap();
+    let item_amount = command.get(3).unwrap();
+
+    // Lookup player by name
+    let user_id = {
+        let mut user_id = None;
+        for (uid, user) in &world.get_resource::<TransportSystem>().unwrap().clients {
+            if &user.name == target_user {
+                user_id = Some(uid);
+                break;
+            }
+        }
+
+        if let Some(user_id) = user_id {
+            *user_id
+        } else {
+            return format!("Invalid user {}", target_user);
+        }
+    };
+
+    // Get item amount
+    let item_amount = {
+        if let Ok(amount) = item_amount.parse::<u32>() {
+            amount
+        } else {
+            return format!("Invalid amount: {}", item_amount);
+        }
+    };
+
+    // Lookup item
+    let item_stack = {
+        let Some((_, item)) = world.get_resource::<ItemStates>().unwrap().get_by_id(target_item) else {
+            return format!("Invalid item: {}", target_item);
+        };
+
+        ItemStack::new(
+            item.clone(),
+            item_amount
+        )
+    };
+
+    // Fetch player inventory
+    let game_object_id = world.get_resource::<TransportSystem>().unwrap().clients.get(&user_id).unwrap().game_object_id.unwrap();
+    let entity = *world.get_resource::<WorldData>().unwrap().game_objects_mapping.get(&game_object_id).unwrap();
+    let mut inventory = world.query::<&mut Inventory>().get_mut(world, entity).unwrap();
+
+    inventory.dirty = true;
+    inventory.push_item(item_stack);
+
+    format!("Added {} {} to {}'s inventory", item_amount, target_item, target_user)
 }
--- server/src/game/join_message.rs
+++ server/src/game/join_message.rs
@@ -19,13 +19,22 @@ pub fn join_message(
 
         let username = game_user.name.clone();
 
-        for (user_id, _) in &users.clients {
+        for (user_id, user) in &users.clients {
             packets.send(SendPacket(Protocol::ChatSent(ChatSent {
                 message: format!("{} joined the game", username)
             }), *user_id));
         }
 
         info!("[Chat] {} joined the game", username);
+
+        let connected_users = users.clients.iter()
+            .map(|(_, v)| v.name.clone())
+            .reduce(|a, b| format!("{}, {}", a, b))
+            .unwrap();
+
+        packets.send(SendPacket(Protocol::ChatSent(ChatSent {
+            message: format!("Connected Users: [{}]", connected_users)
+        }), join_event.id));
     }
 }
 
