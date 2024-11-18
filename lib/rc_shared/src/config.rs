e77a91a70a1a030a9d718b991ce06519c6e94f92
Marshall Scott <daarkzek@protonmail.com>
2024-11-19 08:38:33 +1300

    Temporarily remove dotenvy support

--- lib/rc_shared/src/config.rs
+++ lib/rc_shared/src/config.rs
@@ -1,11 +1,7 @@
 #[macro_export] macro_rules! config {
     ($var:expr) => {
         {
-            use dotenvy_macro::dotenv;
-            match option_env!($var) {
-                Some(value) => value,
-                None => dotenv!($var)
-            }
+            option!($var)
         }
     };
 }
